8ae3aeb253e713cab46d6feed7cab3318e5bf1e7
Ibrahim Hanif - 1302190108 <79637411+cigugur55@users.noreply.github.com>
2022-04-07 09:04:46 +0700

    Progress Sprint #3 (Hanif): Update Informasi Covid

--- lib/infoCovid.dart
+++ lib/infoCovid.dart
@@ -8,12 +8,29 @@ class InfoCovid extends StatefulWidget {
 }
 
 class _InfoCovidState extends State<InfoCovid> {
+
+  final List<String> entries = <String>[
+    'Apa itu Covid-19?',
+    'Apa saja gejala dari Covid-19?',
+    'Apakah Covid-19 ada obatnya?',
+    'Bagaimana cara mencegah diri tertular?'
+  ];
+  final List<int> colorCodes = <int>[50, 100, 50, 100];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('Informasi Covid'),
-      ),
+      body: ListView.builder(
+          padding: const EdgeInsets.all(8),
+          itemCount: entries.length,
+          itemBuilder: (BuildContext context, int index) {
+            return Container(
+              height: 50,
+              color: Colors.blue[colorCodes[index]],
+              child: Center(child: Text('> ${entries[index]}')),
+            );
+          }
+      )
     );
   }
 }
