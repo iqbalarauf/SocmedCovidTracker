57438d784161895ebaf32fc1faf30b411266f710
Adhitya11 <ahendrawan76@gmail.com>
2022-06-15 19:21:49 +0700

    Merubah ukuran

--- lib/statistik.dart
+++ lib/statistik.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 class Statistik extends StatefulWidget {
-  const Statistik({ Key? key }) : super(key: key);
+  const Statistik({Key? key}) : super(key: key);
 
   @override
   State<Statistik> createState() => _StatistikState();
@@ -12,35 +12,33 @@ class _StatistikState extends State<Statistik> {
   Widget build(BuildContext context) {
     return Scaffold(
         body: Column(
-          children: <Widget>[
-
-            Container(
-              child: Text("Terkonfirmasi: 1.816.045 jiwa"),
-              width: 250,
-              color: Colors.orange,
-              padding: EdgeInsets.all(16.0),
-            ),
-            Divider(),
-            Container(
-              child: Text("Dalam Perawatan: 101.650 jiwa"),
-              width: 250,
-              color: Colors.yellow,
-              padding: EdgeInsets.all(16.0),
-            ),
-            Container(
-              child: Text("Sembuh: 1.663.998 jiwa"),
-              width: 250,
-              color: Colors.blue,
-              padding: EdgeInsets.all(16.0),
-            ),
-            Container(
-              child: Text("Wafat: 50.404 jiwa"),
-              width: 250,
-              color: Colors.red,
-              padding: EdgeInsets.all(16.0),
-            ),
-          ],
-        )
-    );
+      children: <Widget>[
+        Container(
+          child: Text("Terkonfirmasi: 1.816.045 jiwa"),
+          width: 300,
+          color: Colors.orange,
+          padding: EdgeInsets.all(16.0),
+        ),
+        Divider(),
+        Container(
+          child: Text("Dalam Perawatan: 101.650 jiwa"),
+          width: 250,
+          color: Colors.yellow,
+          padding: EdgeInsets.all(16.0),
+        ),
+        Container(
+          child: Text("Sembuh: 1.663.998 jiwa"),
+          width: 250,
+          color: Colors.blue,
+          padding: EdgeInsets.all(16.0),
+        ),
+        Container(
+          child: Text("Wafat: 50.404 jiwa"),
+          width: 250,
+          color: Colors.red,
+          padding: EdgeInsets.all(16.0),
+        ),
+      ],
+    ));
   }
 }
