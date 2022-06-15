07a6e06e80efdfaee5142a71c9175dc25a9c05cb
Adhitya11 <ahendrawan76@gmail.com>
2022-06-15 19:25:27 +0700

    Menghapus Gambar pada statistik

--- lib/statistik.dart
+++ lib/statistik.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 class Statistik extends StatefulWidget {
-  const Statistik({ Key? key }) : super(key: key);
+  const Statistik({Key? key}) : super(key: key);
 
   @override
   State<Statistik> createState() => _StatistikState();
@@ -12,44 +12,33 @@ class _StatistikState extends State<Statistik> {
   Widget build(BuildContext context) {
     return Scaffold(
         body: Column(
-          children: <Widget>[
-            Container(
-              color: Colors.white,
-              width: 400,
-              height: 300,
-              padding: EdgeInsets.all(3),
-              child: Image(
-                image: NetworkImage("https://awsimages.detik.net.id/community/media/visual/2020/05/15/2df4fd60-7f64-4325-8f6b-ca357fd2321e.jpeg?w=879"),
-                fit: BoxFit.contain,
-              ),
-            ),
-            Container(
-              child: Text("Terkonfirmasi: 1.816.041 jiwa"),
-              width: 250,
-              color: Colors.orange,
-              padding: EdgeInsets.all(16.0),
-            ),
-            Divider(),
-            Container(
-              child: Text("Dalam Perawatan: 101.639 jiwa"),
-              width: 250,
-              color: Colors.yellow,
-              padding: EdgeInsets.all(16.0),
-            ),
-            Container(
-              child: Text("Sembuh: 1.663.998 jiwa"),
-              width: 250,
-              color: Colors.blue,
-              padding: EdgeInsets.all(16.0),
-            ),
-            Container(
-              child: Text("Wafat: 50.404 jiwa"),
-              width: 250,
-              color: Colors.red,
-              padding: EdgeInsets.all(16.0),
-            ),
-          ],
-        )
-    );
+      children: <Widget>[
+        Container(
+          child: Text("Terkonfirmasi: 1.816.041 jiwa"),
+          width: 250,
+          color: Colors.orange,
+          padding: EdgeInsets.all(16.0),
+        ),
+        Divider(),
+        Container(
+          child: Text("Dalam Perawatan: 101.639 jiwa"),
+          width: 250,
+          color: Colors.yellow,
+          padding: EdgeInsets.all(16.0),
+        ),
+        Container(
+          child: Text("Sembuh: 1.663.998 jiwa"),
+          width: 250,
+          color: Colors.blue,
+          padding: EdgeInsets.all(16.0),
+        ),
+        Container(
+          child: Text("Wafat: 50.404 jiwa"),
+          width: 250,
+          color: Colors.red,
+          padding: EdgeInsets.all(16.0),
+        ),
+      ],
+    ));
   }
 }
